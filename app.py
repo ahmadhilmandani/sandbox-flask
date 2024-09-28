bf7873b2c9efec80df15aa3bb69bd141b842d92a
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-09-28 15:13:54 +0700

    Second commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # part 1: First flask project
 @app.route('/')
@@ -17,5 +17,23 @@ def greetUser():
   greet = request.args.get('greet')
   name = request.args.get('name')
   return f'{greet}, {name}'
+
+#part 3: templates & HTML
+@app.route('/index')
+def index_html():
+  title = "index of page"
+  return render_template('index.html', title=title)
+
+# part 4: Form, Post
+@app.route('/auth', methods=['GET', 'POST'])
+def handle_auth():
+  if request.method == 'GET':
+    return render_template('auth.html', title="Form")
+  elif request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return f'username: {username}\npassword: {password}'
+
+
 if __name__ == "__main__":
   app.run(host='127.0.0.1', debug=True)
